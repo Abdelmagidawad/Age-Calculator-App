45ce86c8c871d59192777d190896aea29325aeff
Abdelmagidawad <abdelmagidawad@gmail.com>
2023-12-27 23:00:22 +0200

    add optien dark&light and loading and resetbtn

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,41 +1,52 @@
 let inputDateBirth = document.querySelector("#date-input");
 let btnCalc = document.querySelector(".calc-btn");
+let btnReset = document.querySelector(".btn-reset");
 let boxYears = document.querySelector(".box .years");
 let boxMonths = document.querySelector(".box .months");
 let boxDays = document.querySelector(".box .days");
 
 btnCalc.addEventListener("click", ageCalculate);
 
+btnReset.addEventListener("click", () => {
+  displayResult("0", "0", "0");
+});
+
 function ageCalculate() {
-  const today = new Date();
-  const inputDate = new Date(inputDateBirth.value);
-  //
-  const birthDateDetails = {
-    date: inputDate.getDate(),
-    month: inputDate.getMonth() + 1,
-    year: inputDate.getFullYear(),
-  };
-  //
-  const currentYear = today.getFullYear();
-  const currentMonth = today.getMonth() + 1;
-  const currentDate = today.getDate();
-  //
-  if (isFutureDate(birthDateDetails, currentYear, currentMonth, currentDate)) {
-    // alert("Not born Yet ");
-    alert("date of birth can't be in the future");
-    displayResult("0", "0", "0");
-    return;
+  if (inputDateBirth.value !== "") {
+    const today = new Date();
+    const inputDate = new Date(inputDateBirth.value);
+    //
+    const birthDateDetails = {
+      date: inputDate.getDate(),
+      month: inputDate.getMonth() + 1,
+      year: inputDate.getFullYear(),
+    };
+    //
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() + 1;
+    const currentDate = today.getDate();
+    //
+    if (
+      isFutureDate(birthDateDetails, currentYear, currentMonth, currentDate)
+    ) {
+      // alert("Not born Yet ");
+      alert("date of birth can't be in the future");
+      displayResult("0", "0", "0");
+      return;
+    }
+    //
+    const { years, months, days } = calculateAge(
+      birthDateDetails,
+      currentYear,
+      currentMonth,
+      currentDate
+    );
+    //
+    displayResult(days, months, years);
+    inputDateBirth.value = "";
+  } else {
+    alert("Please add date of birth");
   }
-  //
-  const { years, months, days } = calculateAge(
-    birthDateDetails,
-    currentYear,
-    currentMonth,
-    currentDate
-  );
-  //
-  displayResult(days, months, years);
-  inputDateBirth.value = "";
 }
 
 function isFutureDate(
@@ -61,23 +72,40 @@ function calculateAge(
 ) {
   let years = currentYear - birthDateDetails.year;
   let months, days;
-  // Calc Months
-  if (currentMonth < birthDateDetails.month) {
+
+  [years, months] = calcMonths(years, currentMonth, birthDateDetails.month);
+  [months, days] = calcDays(
+    months,
+    currentDate,
+    currentMonth,
+    currentYear,
+    birthDateDetails.date
+  );
+  return { years, months, days };
+}
+
+function calcMonths(years, currentMonth, birthMonth) {
+  let months;
+  if (currentMonth < birthMonth) {
     years--;
-    months = 12 - (birthDateDetails.month - currentMonth);
+    months = 12 - (birthMonth - currentMonth);
   } else {
-    months = currentMonth - birthDateDetails.month;
+    months = currentMonth - birthMonth;
   }
-  //Calc Days
-  if (currentDate < birthDateDetails.date) {
+  return [years, months];
+}
+
+function calcDays(months, currentDate, currentMonth, currentYear, birthDate) {
+  let days;
+  if (currentDate < birthDate) {
     months--;
     const lastMonth = currentMonth === 1 ? 12 : currentMonth - 1;
     const daysInlastMonth = getDaysInMonth(lastMonth, currentYear);
-    days = daysInlastMonth - (birthDateDetails.date - currentDate);
+    days = daysInlastMonth - (birthDate - currentDate);
   } else {
-    days = currentDate - birthDateDetails.date;
+    days = currentDate - birthDate;
   }
-  return { years, months, days };
+  return [months, days];
 }
 
 function getDaysInMonth(month, year) {
@@ -111,5 +139,35 @@ let preloadContent = document.querySelector(".preload");
 window.addEventListener("load", () => {
   setTimeout(() => {
     preloadContent.classList.add("hidden");
-  }, 1600);
+  }, 1500);
 });
+
+// Light & Dark Theme
+let btnThem = document.querySelector(".switch");
+let rootElement = document.documentElement;
+
+checkLocalStorage();
+
+btnThem.addEventListener("click", switchThem);
+
+function switchThem() {
+  let dataThem = rootElement.getAttribute("data-theme");
+  let newThem = dataThem === "light" ? "dark" : "light";
+
+  rootElement.setAttribute("data-theme", newThem);
+  btnThem.classList.toggle("trans");
+  // set local Storage
+  localStorage.setItem("them", newThem);
+  localStorage.setItem("IconTransform", btnThem.classList.item(1));
+}
+
+function checkLocalStorage() {
+  if (localStorage.getItem("them")) {
+    rootElement.setAttribute("data-theme", localStorage.getItem("them"));
+  }
+
+  if (localStorage.getItem("IconTransform") === "trans") {
+    btnThem.classList.add("trans");
+  }
+}
+// localStorage.clear();
